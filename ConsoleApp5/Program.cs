590734f1e958701f6da2cf9808e0cb2120712b99
zahrajafari <jafari_zhr@yahoo.com>
2023-04-29 23:57:26 +0330

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class middle
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[50];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i + 1;
+        }
+
+        int middle = GetMiddle(array);
+
+        Console.WriteLine("The middle  is: " + middle);
+    }
+
+    static int GetMiddle(int[] array)
+    {
+        int middleIndex = array.Length / 2;
+        int middle = array[middleIndex];
+        return middle;
+    }
+}
